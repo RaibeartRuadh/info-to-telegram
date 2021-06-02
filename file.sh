4b86c4dfd559b81ab2899251538b49f5dc472bec
RaibeartRuadh <38784332+RaibeartRuadh@users.noreply.github.com>
2021-06-02 12:22:58 +0300

    Update file.sh

--- file.sh
+++ file.sh
@@ -3,10 +3,12 @@
 TOKEN="0000000000:AAEH0000000000000000000000000000"
 CHAT_ID="0000000000"
 
-file1='/home/bs/test.jpg'
+file1='/home/user/test.jpg'
 
 image1="attach://"$(basename $file1)
 
 curl -s -X POST -F media='[{"type":"photo","media":"'$image1'"}]' \
     -F "$(basename $file1)=@${file1}" -H "Content-Type:multipart/form-data" \
     "https://api.telegram.org/bot$TOKEN/sendMediaGroup?chat_id=$CHAT_ID"
+
+
